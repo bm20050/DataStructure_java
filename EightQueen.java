ab92f5ca6f1a07fce4c5034a1ae0406c2b46e57f
Byeongmin <65452989+bm20050@users.noreply.github.com>
2023-02-22 20:14:59 +0900

    Update EightQueen.java

--- EightQueen.java
+++ EightQueen.java
@@ -1,19 +1,23 @@
 package EightQueens;
+
 class Point {
     private int x;
     private int y;
 
-    public Point (int _x, int y) {
+    public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
+
     public int getX() {
         return x;
     }
+
     public int getY() {
         return y;
     }
 }
+
 class MyStack {
     private int top;
     private Point[] data;
@@ -22,30 +26,40 @@ class MyStack {
         top = 0;
         data = new Point[100];
     }
+
+    public boolean isEmpty() {
+        if (top == 0)
+            return true;
+        else
+            return false;
+    }
+
     public void push(Point p) {
         data[top++] = p;
     }
+
     public Point pop() {
         return data[--top];
     }
 }
+
 public class EightQueen {
 
 
     public static boolean checkMove(int x, int y, int[][] array) {
-        if(!(checkRow(x, array))) return false;
-        if(!(checkCol(y, array))) return false;
-        if(!(checkDiagSW(x, y, array))) return false;
-        if(!(checkDiagSE(x, y, array))) return false;
+        if (!(checkRow(x, array))) return false;
+        if (!(checkCol(y, array))) return false;
+        if (!(checkDiagSW(x, y, array))) return false;
+        if (!(checkDiagSE(x, y, array))) return false;
 
         return true;
-     }
+    }
 
     private static boolean checkDiagSE(int x, int y, int[][] array) {
         int dx = x;
         int dy = y;
         while (dx >= 0 && dx < array[y].length && dy >= 0 && dy < array[x].length) {
-            if(array[dx][dy] == 1)
+            if (array[dx][dy] == 1)
                 return false;
             dx++;
             dy--;
@@ -53,7 +67,7 @@ public class EightQueen {
         dx = x;
         dy = y;
         while (dx >= 0 && dx < array[y].length && dy >= 0 && dy < array[x].length) {
-            if(array[dx][dy] == 1)
+            if (array[dx][dy] == 1)
                 return false;
             dx--;
             dy++;
@@ -67,7 +81,7 @@ public class EightQueen {
         int dx = x;
         int dy = y;
         while (dx >= 0 && dx < array[y].length && dy >= 0 && dy < array[x].length) {
-            if(array[dx][dy] == 1)
+            if (array[dx][dy] == 1)
                 return false;
             dx++;
             dy++;
@@ -75,7 +89,7 @@ public class EightQueen {
         dx = x;
         dy = y;
         while (dx >= 0 && dx < array[y].length && dy >= 0 && dy < array[x].length) {
-            if(array[dx][dy] == 1)
+            if (array[dx][dy] == 1)
                 return false;
             dx--;
             dy--;
@@ -99,29 +113,50 @@ public class EightQueen {
         return true;
     }
 
-    public static void solveQueen (int row, int col, int[][] array) {
+    public static void solveQueen(int row, int col, int[][] array) {
         MyStack s = new MyStack();
         Point p = new Point(0, 0);
         int x = p.getX();
         int y = p.getY();
-
-        while (x < row) {
-            while (y < col) {
-                if (checkMove(x, y, array)) {
-                    array[x][y] = 1;
-                    s.push(new Point(x, y));
-                    y = 0;
-                    break;
+        while (true) {
+            while (x < row) {
+                while (y < col) {
+                    if (checkMove(x, y, array)) {
+                        array[x][y] = 1;
+                        s.push(new Point(x, y));
+                        y = 0;
+                        break;
+                    }
+                    y++;
                 }
-                y++;
+                x++;
+                if (y >= col) {
+                    p = s.pop();
+                    x = p.getX();
+                    y = p.getY();
+                    array[x][y] = 0;
+                    y++;
+                }
             }
-            x++;
+            printBoard(row, col, array);
+            p = s.pop();
+            x = p.getX();
+            y = p.getY();
+            array[x][y] = 0;
+            y++;
         }
-
-
-
-
     }
+
+    public static void printBoard(int row, int col, int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                System.out.print(array[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         int row = 4;
         int col = 4;
@@ -132,5 +167,7 @@ public class EightQueen {
             }
         }
         solveQueen(row, col, array);
+
+
     }
 }
