b1d5ff6d8d2aa380db04c42601af0645c295d667
bm20050 <bm20050@gmail.com>
2023-04-12 18:02:44 +0900

    04.12 update

--- ch10_1/Chap11_Test_ChainHash.java
+++ ch10_1/Chap11_Test_ChainHash.java
@@ -1,225 +1,225 @@
-package ch10_1;
-
-import java.util.Comparator;
-//hash node가 student 객체일 때를 구현하는 과제
-//체인법에 의한 해시
-import java.util.Scanner;
-
-class SimpleObject {
-
-    String sno; // 회원번호
-    String sname; // 이름
-
-    public SimpleObject(String sno, String sname) {
-        this.sno = sno;
-        this.sname = sname;
-    }
-    public int keyCode() {
-        return Integer.parseInt(sno);
-    }
-    public String keyValue() {
-        return sname;
-    }
-    // --- 문자열 표현을 반환 ---//
-    public String toString() {
-        return "(" + sno + ") " + sname;
-    }
-
-    // --- 회원번호로 순서를 매기는 comparator ---//
-    public static final Comparator<SimpleObject2> NO_ORDER = new NoOrderComparator();
-
-    private static class NoOrderComparator implements Comparator<SimpleObject2> {
-        public int compare(SimpleObject2 d1, SimpleObject2 d2) {
-            return (d1.sno.compareTo(d2.sno) > 0) ? 1 : ((d1.sno.compareTo(d2.sno) < 0)) ? -1 : 0;
-        }
-    }
-
-    // --- 이름으로 순서를 매기는 comparator ---//
-    public static final Comparator<SimpleObject2> NAME_ORDER = new NameOrderComparator();
-
-    private static class NameOrderComparator implements Comparator<SimpleObject2> {
-        public int compare(SimpleObject2 d1, SimpleObject2 d2) {
-            return (d1.sname.compareTo(d2.sname) > 0) ? 1 : ((d1.sname.compareTo(d2.sname) < 0)) ? -1 : 0;
-        }
-    }
-}
-
-class ChainHash2 {
-    //--- 해시를 구성하는 노드 ---//
-    class Node2 {
-        private SimpleObject data;                 // 키값
-        private Node2 next;        // 뒤쪽 포인터(뒤쪽 노드에 대한 참조)
-
-        //--- 생성자(constructor) ---//
-        public Node2(SimpleObject s) {
-            this.data = new SimpleObject(s);
-            this.next = null;
-        }
-
-        Node2(SimpleObject s, Node2 p) {
-            this.data = s;
-            this.next = p;
-
-        }
-
-        Node2() {
-            this.data = null;
-            this.next = null;
-        }
-
-        //--- 키값을 반환 ---//
-        Integer getKey() {
-            return data.keyCode();
-        }
-        String getValue() {
-            return data.keyValue();
-        }
-
-        //--- 키의 해시값을 반환 ---//
-        public int hashCode() {
-            int hash = 11;
-            hash = 31 * hash * getKey();
-            hash = hash * hash;
-            return hash;
-        }
-    }
-
-    private int size;              // 해시 테이블의 크기
-    private Node2[] table;        // 해시 테이블
-
-    //--- 생성자(constructor) ---//
-    public ChainHash2(int capacity) {
-        try {
-            table = new Node2[capacity];
-            this.size = capacity;
-        } catch (OutOfMemoryError e) {        // 테이블을 생성할 수 없음
-            this.size = 0;
-        }
-    }
-
-    //--- 해시값을 구함 ---//
-    public int hashValue(Object key) {
-        int hash = 11;
-        hash = 31 * hash * (int) key;
-        hash = hash * hash;
-        return hash;
-
-
-    }
-
-    //--- 키값이 key인 요소를 검색(데이터를 반환) ---//
-    public String search(int key) {
-        int hash = hashValue(key);            // 검색할 데이터의 해시값
-        Node2 p = table[hash];            // 선택 노드
-
-        while (p != null) {
-            if (p.getKey() == key)
-                return p.getValue();                // 검색 성공
-            p = p.next;                             // 다음 노드를 선택
-        }
-        return null;                                // 검색 실패
-    }
-
-    //--- 키값이 key인 데이터를 data의 요소로 추가 ---//
-    public int add(SimpleObject st) {
-        int hash = hashValue(st.keyCode()) % 9;            // 추가할 데이터의 해시값
-        Node2 p = table[hash];            // 선택 노드
-//구현 필요함
-        while (p != null) {
-            if (p.getKey() == Integer.parseInt(st.sno))
-                return 1;
-            p = p.next;
-        }
-        Node2 temp = new Node2(st, table[hash]);
-        table[hash] = temp;
-        return 0;
-    }
-
-    //--- 키값이 key인 요소를 삭제 ---//
-    public int remove(int key) {
-        int hash = hashValue(key);            // 삭제할 데이터의 해시값
-        Node2 p = table[hash];            // 선택 노드
-        Node2 pp = null;                  // 바로 앞의 선택 노드
-        //구현실습
-
-
-        return 1;                             // 찾는 키값이 없음
-    }
-
-    //--- 해시 테이블을 덤프(dump) ---//
-    public void dump() {
-        for (int i = 0; i < size; i++) {
-            Node2 p = table[i];
-            System.out.printf("%02d  ", i);
-            while (p != null) {
-                System.out.printf("→ %s ", p.getKey());
-                p = p.next;
-            }
-            System.out.println();
-        }
-    }
-}
-
-
-public class Chap11_Test_ChainHash {
-    static Scanner stdIn = new Scanner(System.in);
-
-    public static void main(String[] args) {
-        ChainHash2 hash = new ChainHash2(11);
-        SimpleObject data;
-        int select = 0;
-        final int count = 3;
-        while (select != 6) {
-            System.out.println(1);
-            System.out.println(
-                    "SimpleChainHash. Select 1:Add, 2. Delete, 3:Search, 4. PrintDump, 5. Quit =>");
-
-            select = stdIn.nextInt();
-            switch (select) {
-                case 1:
-                    SimpleObject[] input = new SimpleObject[count];
-                    String sno = null;
-                    String sname = null;
-                    for (int ix = 0; ix < count; ix++) {
-
-                        System.out.println("입력 데이터(sno, sname):: ");
-
-                        System.out.print("번호: ");
-                        sno = stdIn.next();
-
-                        System.out.print("이름: ");
-                        sname = stdIn.next();
-                        System.out.print("sno =  " + sno);
-                        input[ix] = new SimpleObject(sno, sname);
-                        hash.add(input[ix]);
-
-                        System.out.print(" " + input[ix]);
-                    }
-                    break;
-                case 2:
-                    // Delete
-                    break;
-                case 3:
-                    System.out.println("Search Value:: ");
-                    int val = stdIn.nextInt();
-                    String v = hash.search(val);
-                    if (v != null)
-                        System.out.println(v);
-                    else
-                        System.out.println("데이터가 없습니다.");
-                    break;
-                case 4:
-                    hash.dump();
-                    break;
-                case 5:
-                    System.out.println("Quit");
-                    break;
-
-                default:
-                    System.out.println("WRONG INPUT  ");
-                    System.out.println("Re-Enter");
-                    break;
-            }
-        }
-    }
-}
+//package ch10_1;
+//
+//import java.util.Comparator;
+////hash node가 student 객체일 때를 구현하는 과제
+////체인법에 의한 해시
+//import java.util.Scanner;
+//
+//class SimpleObject {
+//
+//    String sno; // 회원번호
+//    String sname; // 이름
+//
+//    public SimpleObject(String sno, String sname) {
+//        this.sno = sno;
+//        this.sname = sname;
+//    }
+//    public int keyCode() {
+//        return Integer.parseInt(sno);
+//    }
+//    public String keyValue() {
+//        return sname;
+//    }
+//    // --- 문자열 표현을 반환 ---//
+//    public String toString() {
+//        return "(" + sno + ") " + sname;
+//    }
+//
+//    // --- 회원번호로 순서를 매기는 comparator ---//
+//    public static final Comparator<SimpleObject2> NO_ORDER = new NoOrderComparator();
+//
+//    private static class NoOrderComparator implements Comparator<SimpleObject2> {
+//        public int compare(SimpleObject2 d1, SimpleObject2 d2) {
+//            return (d1.sno.compareTo(d2.sno) > 0) ? 1 : ((d1.sno.compareTo(d2.sno) < 0)) ? -1 : 0;
+//        }
+//    }
+//
+//    // --- 이름으로 순서를 매기는 comparator ---//
+//    public static final Comparator<SimpleObject2> NAME_ORDER = new NameOrderComparator();
+//
+//    private static class NameOrderComparator implements Comparator<SimpleObject2> {
+//        public int compare(SimpleObject2 d1, SimpleObject2 d2) {
+//            return (d1.sname.compareTo(d2.sname) > 0) ? 1 : ((d1.sname.compareTo(d2.sname) < 0)) ? -1 : 0;
+//        }
+//    }
+//}
+//
+//class ChainHash2 {
+//    //--- 해시를 구성하는 노드 ---//
+//    class Node2 {
+//        private SimpleObject data;                 // 키값
+//        private Node2 next;        // 뒤쪽 포인터(뒤쪽 노드에 대한 참조)
+//
+//        //--- 생성자(constructor) ---//
+//        public Node2(SimpleObject s) {
+//            this.data = new SimpleObject(s);
+//            this.next = null;
+//        }
+//
+//        Node2(SimpleObject s, Node2 p) {
+//            this.data = s;
+//            this.next = p;
+//
+//        }
+//
+//        Node2() {
+//            this.data = null;
+//            this.next = null;
+//        }
+//
+//        //--- 키값을 반환 ---//
+//        Integer getKey() {
+//            return data.keyCode();
+//        }
+//        String getValue() {
+//            return data.keyValue();
+//        }
+//
+//        //--- 키의 해시값을 반환 ---//
+//        public int hashCode() {
+//            int hash = 11;
+//            hash = 31 * hash * getKey();
+//            hash = hash * hash;
+//            return hash;
+//        }
+//    }
+//
+//    private int size;              // 해시 테이블의 크기
+//    private Node2[] table;        // 해시 테이블
+//
+//    //--- 생성자(constructor) ---//
+//    public ChainHash2(int capacity) {
+//        try {
+//            table = new Node2[capacity];
+//            this.size = capacity;
+//        } catch (OutOfMemoryError e) {        // 테이블을 생성할 수 없음
+//            this.size = 0;
+//        }
+//    }
+//
+//    //--- 해시값을 구함 ---//
+//    public int hashValue(Object key) {
+//        int hash = 11;
+//        hash = 31 * hash * (int) key;
+//        hash = hash * hash;
+//        return hash;
+//
+//
+//    }
+//
+//    //--- 키값이 key인 요소를 검색(데이터를 반환) ---//
+//    public String search(int key) {
+//        int hash = hashValue(key);            // 검색할 데이터의 해시값
+//        Node2 p = table[hash];            // 선택 노드
+//
+//        while (p != null) {
+//            if (p.getKey() == key)
+//                return p.getValue();                // 검색 성공
+//            p = p.next;                             // 다음 노드를 선택
+//        }
+//        return null;                                // 검색 실패
+//    }
+//
+//    //--- 키값이 key인 데이터를 data의 요소로 추가 ---//
+//    public int add(SimpleObject st) {
+//        int hash = hashValue(st.keyCode()) % 9;            // 추가할 데이터의 해시값
+//        Node2 p = table[hash];            // 선택 노드
+////구현 필요함
+//        while (p != null) {
+//            if (p.getKey() == Integer.parseInt(st.sno))
+//                return 1;
+//            p = p.next;
+//        }
+//        Node2 temp = new Node2(st, table[hash]);
+//        table[hash] = temp;
+//        return 0;
+//    }
+//
+//    //--- 키값이 key인 요소를 삭제 ---//
+//    public int remove(int key) {
+//        int hash = hashValue(key);            // 삭제할 데이터의 해시값
+//        Node2 p = table[hash];            // 선택 노드
+//        Node2 pp = null;                  // 바로 앞의 선택 노드
+//        //구현실습
+//
+//
+//        return 1;                             // 찾는 키값이 없음
+//    }
+//
+//    //--- 해시 테이블을 덤프(dump) ---//
+//    public void dump() {
+//        for (int i = 0; i < size; i++) {
+//            Node2 p = table[i];
+//            System.out.printf("%02d  ", i);
+//            while (p != null) {
+//                System.out.printf("→ %s ", p.getKey());
+//                p = p.next;
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+//
+//
+//public class Chap11_Test_ChainHash {
+//    static Scanner stdIn = new Scanner(System.in);
+//
+//    public static void main(String[] args) {
+//        ChainHash2 hash = new ChainHash2(11);
+//        SimpleObject data;
+//        int select = 0;
+//        final int count = 3;
+//        while (select != 6) {
+//            System.out.println(1);
+//            System.out.println(
+//                    "SimpleChainHash. Select 1:Add, 2. Delete, 3:Search, 4. PrintDump, 5. Quit =>");
+//
+//            select = stdIn.nextInt();
+//            switch (select) {
+//                case 1:
+//                    SimpleObject[] input = new SimpleObject[count];
+//                    String sno = null;
+//                    String sname = null;
+//                    for (int ix = 0; ix < count; ix++) {
+//
+//                        System.out.println("입력 데이터(sno, sname):: ");
+//
+//                        System.out.print("번호: ");
+//                        sno = stdIn.next();
+//
+//                        System.out.print("이름: ");
+//                        sname = stdIn.next();
+//                        System.out.print("sno =  " + sno);
+//                        input[ix] = new SimpleObject(sno, sname);
+//                        hash.add(input[ix]);
+//
+//                        System.out.print(" " + input[ix]);
+//                    }
+//                    break;
+//                case 2:
+//                    // Delete
+//                    break;
+//                case 3:
+//                    System.out.println("Search Value:: ");
+//                    int val = stdIn.nextInt();
+//                    String v = hash.search(val);
+//                    if (v != null)
+//                        System.out.println(v);
+//                    else
+//                        System.out.println("데이터가 없습니다.");
+//                    break;
+//                case 4:
+//                    hash.dump();
+//                    break;
+//                case 5:
+//                    System.out.println("Quit");
+//                    break;
+//
+//                default:
+//                    System.out.println("WRONG INPUT  ");
+//                    System.out.println("Re-Enter");
+//                    break;
+//            }
+//        }
+//    }
+//}
--- t/MinimalSpanningTree.java
+++ t/MinimalSpanningTree.java
@@ -1,46 +1,296 @@
 package t;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 class Element {
+    private int key;
 
+    public int getKey () {
+        return key;
+    }
 }
 
-class MaxPQ {
-
+interface MaxPQ {
+    public void insert(Element o);
+    public Element deleteMax(Element o);
 }
 
-class MaxHeap {
+class MaxHeap implements MaxPQ{
+    private Element[] heap;
+    private int n;
+    private int maxSize;
+    public MaxHeap(int sz) {
+        maxSize = sz;
+    }
 
+    public void heapEmpty() {
+        System.out.println("Heap Empty");
+    }
+
+    public void heapFull() {
+        System.out.println("Heap Full");
+    }
+
+    public void display() {
+        System.out.print("MaxHeap:: (i, heap[i].key):");
+        for (int i = 1; i <= n; i++) {
+            System.out.println("(" + i + ", " + heap[i].getKey() + ")");
+        }
+    }
+
+    public void insert(Element x) {
+        if (n == maxSize) {
+            heapFull();
+            return;
+        }
+        n++;
+        int i;
+        for (i = n; i != 1 ; i /= 2) {
+            if (x.getKey() <= heap[i / 2].getKey())
+                break;
+            heap[i] = heap[i / 2];
+        }
+        heap[i] = x;
+    }
+
+    public Element deleteMax(Element x) {
+        int i, j;
+        if (n == 0) {
+            heapEmpty();
+            return null;
+        }
+        x = heap[1];
+        Element k = heap[n--];
+        for (i = 1, j = 2; j <= n; i = j, j *= 2) {
+            if (j < n && heap[j].getKey() < heap[j + 1].getKey())
+                j++;
+            if (k.getKey() >= heap[j].getKey())
+                break;
+            heap[i] = heap[j];
+        }
+        heap[i] = k;
+        return x;
+    }
 }
 
 class Sets {
+    private int[] parent;
+    private int n;
 
+    public Sets(int sz) {
+        n = sz;
+        parent = new int[sz + 1];
+        for (int i = 1; i <= n; i++) {
+            parent[i] = -1;
+        }
+    }
+
+    public void simpleUnion(int i, int j) {
+        parent[j] = i;
+    }
+
+    public int simpleFind(int i) {
+        while(parent[i] > 0) {
+            i = parent[i];
+        }
+        return i;
+    }
+
+    public void weightedUnion(int i, int j) {
+        int temp = parent[i] + parent[j];
+        if (parent[i] > parent[j]) {
+            parent[i] = j;
+            parent[j] = temp;
+        }
+        else {
+            parent[j] = i;
+            parent[i] = temp;
+        }
+    }
+
+    public int collapsingFind(int i) {
+        int r;
+        for (r = i; parent[r] > 0; r = parent[r]);
+
+        while (i != r) {
+            int s = parent[i];
+            parent[i] = r;
+            i = s;
+        }
+
+        return r;
+    }
+
+    public void display() {
+        System.out.print("display: index= ");
+        for (int i = 1; i <= n; i++)
+            System.out.print(i + " ");
+        System.out.println();
+
+        System.out.print("dispaly: index= ");
+        for (int i = 1; i <= n; i++)
+            System.out.print(parent[i] + " ");
+        System.out.println();
+    }
 }
 
 class ListNode {
+    private int data;
+    ListNode link;
 
-}
-
-class ListIterator {
-
+    public ListNode(int data) {
+        this.data = data;
+        link = null;
+    }
+    public int getData() {
+        return data;
+    }
 }
 
 class List {
+    ListNode first;
 
+    public void insert(int k) {
+        ListNode newnode = new ListNode(k);
+        newnode.link = first;
+        first = newnode;
+    }
+
+    public void delete(int k) {
+        ListNode previous = null;
+        ListNode current;
+        for (current = first; current.getData() != k && current != null;
+                    previous = current, current = current.link);
+        if (current != null) {
+            if (previous != null)
+                previous.link = current.link;
+            else
+                first = first.link;
+        }
+    }
 }
 
+class ListIterator {
+    private List list;
+    ListNode current;
+
+    public ListIterator(List l) {
+        current = l.first;
+        ListNode current;
+    }
+
+    public int first() {
+        if (current != null)
+            return current.getData();
+        else
+            return 0;
+    }
+
+    public int next() {
+        current = current.link;
+        return current.getData();
+    }
+
+    public boolean notNull() {
+        if (current != null)
+            return true;
+        else
+            return false;
+    }
+
+    public boolean nextNotNull() {
+        if (current.link != null)
+            return true;
+        else
+            return false;
+    }
+}
+
+
+
 class QueueNode {
+    int data;
+    QueueNode link;
+
+    public QueueNode() {
+        data = 0;
+        link = null;
+    }
+    public QueueNode(int def, QueueNode l) {
+        data = def;
+        link = l;
+    }
 
 }
 
 class Queue {
+    private QueueNode front;
+    private QueueNode rear;
 
+    public Queue() {
+        front = rear = null;
+    }
+
+    public void insert(int y) {
+        if (front == null)
+            front = rear = new QueueNode(y, null);
+        else {
+            rear.link = new QueueNode();
+            rear = rear.link;
+        }
+    }
+
+    public int delete(int retvalue) {
+        if (front == 0) {
+            queueEmpty();
+            return 0;
+        }
+        QueueNode x = front;
+        retvalue = front.data;
+        front = x.link;
+        if (front == null)
+            rear = null;
+        // delete x;
+        return retvalue;
+    }
+
+    public boolean isEmpty() {
+        if (front == null)
+            return true;
+        else
+            return false;
+    }
 }
 
 class Graph {
+    List headNodes;
+    private int n;
+    boolean[] visited;
 
+    public Graph(int vertices) {
+        headNodes = new List();
+    }
+    public void _DFS(int v) {
+
+    }
 }
 
 public class MinimalSpanningTree {
     public static void main(String[] args) {
+        int select = 0, n, start, end, weight;
+        int startBFSNode = 100;
 
+        System.out.println("Input the total node number: ");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        Graph graph(n);
+        Graph[] SpanningTree = null;
+        while (select != 0) {
+            System.out.print("Select command 1: Add edges and Weight, 2: Display Adjacency Lists, 3: spanningTree, 4: Quit => ");
+            select = sc.nextInt();
+
+        }
     }
 }
